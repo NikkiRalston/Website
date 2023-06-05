70dcb8bc2bdc083b5b37bfa9e4cdd6f9b88449ef
NikkiRalston <94995862+NikkiRalston@users.noreply.github.com>
2023-06-05 13:31:31 -0700

    Add files via upload

---
+++ Mars.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mars Tours</title>
+    <link rel="stylesheet" type="text/css" href="Mars.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Mars Tours</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Tours</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <section>
+            <h2>About Mars Tours</h2>
+            <p>Mars Tours is a pioneering company that offers unforgettable tours to the mysterious and breathtaking planet of Mars. Our mission is to provide a once-in-a-lifetime experience for space enthusiasts, explorers, and adventurers.</p>
+        </section>
+    </main>
+    <script src="Mars.js"></script>
+</body>
+</html>
